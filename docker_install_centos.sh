24d1e33f83d2eede471a4be872838b3e12ef99ec
rendybomber <35839822+rendybomber@users.noreply.github.com>
2020-02-26 11:31:50 +0700

    v1

--- docker_install_centos.sh
+++ docker_install_centos.sh
@@ -4,7 +4,7 @@
 #Author : Randi Sunarsa
 #Copyright : 2020
 echo '--------------------------------'
-echo '###_Please wait for Docker Installation_###'
+echo '####_Please wait for Docker Installation_####'
 echo '--------------------------------'
 echo 'Dependencies installation   .'
 yum install -y yum-utils device-mapper-persistent-data lvm2
