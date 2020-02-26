0cd2d5ef8089a89ac751c0ec710ed60323b25b10
root <root@OGAPMDYNAGATE01.oto.co.id>
2020-02-26 10:36:13 +0700

    v1

---
+++ docker_install_CentOS.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Category : Bash Script
+#Content : Docker Installation for CentOS 7
+#Author : Randi Sunarsa
+#Copyright : 2020
+echo '--------------------------------'
+echo '###_Please wait for Docker Installation_###'
+echo '--------------------------------'
+echo 'Dependencies installation   .'
+yum install -y yum-utils device-mapper-persistent-data lvm2
+echo '--------------------------------'
+echo 'Adding Docker Repository   .'
+yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+echo '--------------------------------'
+echo 'Docker installation   .'
+yum install docker-ce
+echo '--------------------------------'
+echo 'Starting Docker Service  .'
+systemctl start docker
+systemctl enable docker
+echo 'Docker installation done and ready to use   .'
